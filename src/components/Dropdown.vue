89e85d60452a74deedba56a17f60532563a09a5e
trojan0523 <1153532327@qq.com>
2021-01-04 20:14:53 +0800

    first commit

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dropdown">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle">{{ title }}</a>
+    <ul class="dropdown-menu">
+      <li class="dropdown-item">
+        <a href="#">新建文章</a>
+      </li>
+      <li class="dropdown-item">
+        <a href="#">编辑资料</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Dropdowm'
+})
+</script>
+
+<style></style>
