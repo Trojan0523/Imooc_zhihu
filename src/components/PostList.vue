7aa881f8fb9f197199f555b5283f4d0467eb6b0a
trojan0523 <1153532327@qq.com>
2021-01-05 16:01:49 +0800

    ColumnDetail and PostList finished

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <article v-for="post in list" :key="post._id" class="card mb-3 shadow-sm">
+      <div class="card-body">
+        <h4><router-link :to="`/posts/${post.id}/`">{{post.title}}</router-link></h4>
+        <div class="row my-3 align-items-center">
+        </div>
+        <span class="text-muted">{{post.createdAt}}</span>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { PostProps } from '@/testData'
+export default defineComponent({
+  props: {
+    list: {
+      required: true,
+      type: Array as PropType<PostProps[]>
+    }
+  }
+})
+</script>
--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,18 +1,36 @@
 <template>
-  <div>
-    <pre>{{ route }}</pre>
+  <div class="column-detail-page w-75 mx-auto">
+    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+      <div class="col-3 text-center">
+        <img :src="column.avatar" :alt="column.title" class="rounded-circle">
+      </div>
+      <div class="col-9">
+        <h4>{{ column.title }}</h4>
+        <p class="text-muted">{{ column.description }}</p>
+      </div>
+    </div>
+    <PostList :list="list"></PostList>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
-
+import { testData, testPosts } from '@/testData'
+import PostList from '@/components/PostList.vue'
 export default defineComponent({
+  components: {
+    PostList
+  },
   setup () {
     const route = useRoute()
+    const currentId = +route.params.id
+    const column = testData.find(c => c.id === currentId)
+    const list = testPosts.filter(post => post.id === currentId)
+    console.log(list)
     return {
-      route
+      column,
+      list
     }
   }
 
